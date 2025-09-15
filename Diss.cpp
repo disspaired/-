1e35825e75f8c70e0fd2cca6d21de8d11bbdf02f
disspaired <alehrom10092007@gmail.com>
2025-09-15 10:37:20 +0300

    Create Diss.cpp

---
+++ Diss.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main()
+{
+   int year = 2024;
+   std::cout << year << std::endl;
+   
+   double pi = 3.14159;
+   std::cout << pi << std::endl;
+   
+   char grade = 'A';
+   std::cout << grade << std::endl;
+   
+   bool IsRainy = true;
+   std::cout << IsRainy << std::endl;
+   
+   float price = 99.99f;
+   std::cout << price << std::endl;
+   
+   int number;
+   number = 50;
+   std::cout << number << std::endl;
+   
+   int a, b, c;
+   
+   double width = 15.5;
+   double height = 25.7;
+   
+   std::cout << width << std::endl;
+   std::cout << height << std::endl;
+   
+   char initial = 'A';
+   std::cout << initial << std::endl;
+   
+   bool IsGameOver = false;
+   std::cout << IsGameOver << std::endl;
+   
+ return 0;   
+}
